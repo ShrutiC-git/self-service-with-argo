792f966b3bcd3e084dc4d90e733cb1fd4171dff4
Shruti Chaturvedi <66940685+ShrutiC-git@users.noreply.github.com>
2025-09-20 13:18:23 +0530

    changed job definition

--- workloads/jobs/data-processing.go
+++ workloads/jobs/data-processing.go
@@ -7,7 +7,7 @@ import (
 )
 
 func main() {
-	fmt.Printf("Data processing job started at %v\n", time.Now())
+	fmt.Printf("Data processing job started at %v...\n", time.Now())
 	for task := 1; task <= 5; task++ {
 		processTask(task)
 	}
@@ -15,8 +15,8 @@ func main() {
 }
 
 func processTask(task int) {
-	duration := rand.IntN(3) + 1
-	fmt.Printf("Processing task %d for %d seconds\n", task, duration)
+	duration := rand.IntN(5) + 1
+	fmt.Printf("Processing task %d for %d seconds...\n", task, duration)
 	time.Sleep(time.Duration(duration) * time.Second)
 	fmt.Printf("Task %d processed\n", task)
 }

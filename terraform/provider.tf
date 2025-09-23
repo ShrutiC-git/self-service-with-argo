443210d1a4d95af554109065cf55b77ab8fe1ead
Shruti Chaturvedi <66940685+ShrutiC-git@users.noreply.github.com>
2025-09-23 11:37:38 +0530

    add providers

---
+++ terraform/provider.tf
@@ -0,0 +1,26 @@
+terraform {
+  required_providers {
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = "~> 2.32"
+    }
+    helm = {
+      source  = "hashicorp/helm"
+      version = "~> 2.11.0"
+    }
+  }
+   required_version = ">= 1.5.0"
+}
+
+provider "kubernetes" {
+  config_path    = pathexpand("~/.kube/config")
+  config_context = "kind-self-service"
+}
+
+provider "helm" {
+  kubernetes {
+    config_path    = pathexpand("~/.kube/config")
+    config_context = "kind-self-service"
+  }
+}
+
--- terraform/storage.tf
+++ terraform/storage.tf
@@ -1,7 +1,15 @@
 resource "kubernetes_persistent_volume_claim" "artifacts" {
-  metadata { name="model-artifacts" namespace="ai-jobs" }
+  metadata {
+    name      = "model-artifacts"
+    namespace = kubernetes_namespace.ns["ai-jobs"].metadata[0].name
+  }
   spec {
     access_modes = ["ReadWriteOnce"]
-    resources { requests = { storage = "5Gi" } }
+    resources {
+      requests = {
+        storage = "1Gi"
+      }
+    }
+    storage_class_name = "standard"
   }
 }
